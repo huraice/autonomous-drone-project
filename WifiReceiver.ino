e77b27926c27cfa84fb0379cbcf761b0b8c375a2
Huraice S <71816082+huraice@users.noreply.github.com>
2023-04-30 05:31:01 +0530

    Add files via upload

---
+++ WifiReceiver.ino
@@ -0,0 +1,84 @@
+
+
+// Replace with your network credentials
+const char* ssid = "LAPTOP-OIG4EU77 7939";
+const char* password = "7Gp8535<";
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <Mavlink.h>
+//// Replace with your network credentials
+//const char* ssid = "your_SSID";
+//const char* password = "your_PASSWORD";
+
+// Port number to listen for data on
+const unsigned int localPort = 12345;
+
+// UDP object
+WiFiUDP udp;
+
+void setup() {
+  // Connect to Wi-Fi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  // Start UDP server
+  udp.begin(localPort);
+  Serial.print("Listening on port ");
+  Serial.println(localPort);
+}
+
+void loop() {
+  // Buffer to hold incoming data
+  char packetBuffer[255];
+
+  // Receive incoming UDP packets
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    // Read data into buffer
+    int len = udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+
+    // Parse joystick data
+    int roll = 0;
+    int pitch = 0;
+    int throttle = 0;
+    int yaw = 0;
+
+    sscanf(packetBuffer, "%d,%d,%d,%d", &roll, &pitch, &throttle, &yaw);
+
+    // Send RC commands to Pixhawk
+    int RC_CHANNELS[8] = {1500, 1500, throttle, yaw, roll, pitch, 1500, 1500};
+    int start_channel = 1;
+    for (int i = 0; i < 8; i++) {
+      int pwm = RC_CHANNELS[i];
+      if (pwm < 1000) pwm = 1000;
+      if (pwm > 2000) pwm = 2000;
+      pwm = map(pwm, 1000, 2000, 1100, 1900);
+      pwm = constrain(pwm, 1100, 1900);
+      pwm = round(pwm);
+      pwm = (pwm - 1100) * 2 + 1000;
+      pwm = constrain(pwm, 1000, 2000);
+      pwm = map(pwm, 1000, 2000, 0, 65535);
+      uint16_t channel = start_channel + i;
+      mavlink_msg_rc_channels_override_send(MAVLINK_COMM_0, 1, channel, pwm);
+    }
+
+    // Print joystick values
+    Serial.print("Roll: ");
+    Serial.print(roll);
+    Serial.print(", Pitch: ");
+    Serial.print(pitch);
+    Serial.print(", Throttle: ");
+    Serial.print(throttle);
+    Serial.print(", Yaw: ");
+    Serial.println(yaw);
+  }
+
+  delay(10);
+}
---
+++ WifiTransmitter.ino
@@ -0,0 +1,78 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+// Replace with your network credentials
+const char* ssid = "LAPTOP-OIG4EU77 7939";
+const char* password = "7Gp8535<";
+
+// IP address of the receiver
+const IPAddress receiverIP(192, 168, 1, 100);
+
+// Port number to send data to
+const unsigned int receiverPort = 12345;
+
+// Define pins for joystick 1
+#define joy1XPin  A0 // Arduino pin connected to VRX pin
+//#define joy1YPin  A1
+#define joy1YPin D4
+//const int joy1XPin = A0;
+//const int joy1YPin = A1;
+
+// Define pins for joystick 2
+#define joy2XPin   D3 // Arduino pin connected to VRX pin
+#define joy2YPin  D0
+//const int joy2XPin =;
+//const int joy2YPin = ;
+
+// Joystick threshold
+const int joyThreshold = 10;
+
+// UDP object
+WiFiUDP udp;
+
+void setup() {
+  Serial.begin(115200);
+  // Connect to Wi-Fi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  // Print local IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Read joystick 1 input
+  int joy1X = analogRead(joy1XPin);
+  int joy1Y = analogRead(joy1YPin);
+
+  // Read joystick 2 input
+  int joy2X = analogRead(joy2XPin);
+  int joy2Y = analogRead(joy2YPin);
+
+  // Calculate joystick 1 values
+  int joy1XVal = map(joy1X, 0, 1023, -100, 100);
+  int joy1YVal = map(joy1Y, 0, 1023, -100, 100);
+
+  // Calculate joystick 2 values
+  int joy2XVal = map(joy2X, 0, 1023, -100, 100);
+  int joy2YVal = map(joy2Y, 0, 1023, -100, 100);
+
+  // Check joystick thresholds
+  if (abs(joy1XVal) < joyThreshold) joy1XVal = 0;
+  if (abs(joy1YVal) < joyThreshold) joy1YVal = 0;
+  if (abs(joy2XVal) < joyThreshold) joy2XVal = 0;
+  if (abs(joy2YVal) < joyThreshold) joy2YVal = 0;
+
+  // Create data packet
+  String data = String(joy1XVal) + "," + String(joy1YVal) + "," + String(joy2XVal) + "," + String(joy2YVal);
+
+  // Send data to receiver
+  udp.beginPacket(receiverIP, receiverPort);
+  udp.print(data);
+  udp.endPacket();
+
+  delay(10);
+}
